6c7fd32c7c4dce3945fe05af7480b7ea3586b026
Christian R <christianriis.kodehode@gmail.com>
2023-01-05 08:51:42 +0100

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+// COMPONENTS
+import Navbar from "./components/Navbar.jsx";
+
+// CSS
+import "./styles/global.css";
+import "./styles/palette.css";
+import "./styles/typography.css";
+import "./styles/sizes.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar className="navbar">
+        <a className="navitems" href="#">
+          About
+        </a>
+        <a className="navitems" href="#">
+          Projects
+        </a>
+        <a className="navitems" href="#">
+          Contact
+        </a>
+      </Navbar>
+      <h1>Hei</h1>
+      <h2>Portfolio</h2>
+      <h3>test</h3>
+    </div>
+  );
+}
+
+export default App;
